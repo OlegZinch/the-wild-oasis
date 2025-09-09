0b22343422602d002fcdb23c12bc4eefa0309549
Oleh Zinchenko <olegzinchenko1983@gmail.com>
2025-09-09 19:57:32 +0100

    Applying the Render Props Pattern

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -19,9 +19,10 @@ function CabinTable() {
         <div></div>
       </Table.Header>
 
-      {cabins.map((cabin) => (
-        <CabinRow key={cabin.id} cabin={cabin} />
-      ))}
+      <Table.Body
+        data={cabins}
+        render={(cabin) => <CabinRow key={cabin.id} cabin={cabin} />}
+      />
     </Table>
   )
 }
