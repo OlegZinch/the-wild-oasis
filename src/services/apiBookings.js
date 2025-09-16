b70116406c21ef26a20485e66e5a0fa19c21bfed
Oleh Zinchenko <olegzinchenko1983@gmail.com>
2025-09-16 19:22:35 +0100

    API-Side Sorting: Sorting Bookings

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -8,8 +8,11 @@ export async function getBookings({ filter, sortBy }) {
       'id, created_at, startDate, endDate, numNights, numGuests, totalPrice, status, cabins(name), guests(fullName, email)'
     )
   // FILTER
-  if (filter !== null)
-    query = query[filter.method || 'eq'](filter.field, filter.value)
+  if (filter) query = query[filter.method || 'eq'](filter.field, filter.value)
+
+  // SORT
+  if (sortBy)
+    query = query.order(sortBy.field, { ascending: sortBy.direction === 'asc' })
 
   const { data, error } = await query
 
