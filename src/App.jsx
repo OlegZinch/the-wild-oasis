f207fc293c5896b7fa6fdeacc7fa0f313d02404d
Oleh Zinchenko <olegzinchenko1983@gmail.com>
2025-10-03 17:53:49 +0100

    Implementing Dark Mode With CSS Variables

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ import Checkin from './pages/Checkin'
 import PageNotFound from './pages/PageNotFound'
 import AppLayout from './ui/AppLayout'
 import ProtectedRoute from './ui/ProtectedRoute'
+import { DarkModePovider } from './context/DarkModeContext'
 
 // Create a client
 const queryClient = new QueryClient({
@@ -29,57 +30,59 @@ const queryClient = new QueryClient({
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <GlobalStyles />
-      <BrowserRouter>
-        <Routes>
-          <Route
-            element={
-              <ProtectedRoute>
-                <AppLayout />
-              </ProtectedRoute>
-            }
-          >
-            <Route index element={<Navigate replace to='dashboard' />} />
-            <Route path='dashboard' element={<Dashboard />} />
-            <Route path='bookings' element={<Bookings />} />
-            <Route path='bookings/:bookingId' element={<Booking />} />
-            <Route path='checkin/:bookingId' element={<Checkin />} />
+    <DarkModePovider>
+      <QueryClientProvider client={queryClient}>
+        <GlobalStyles />
+        <BrowserRouter>
+          <Routes>
+            <Route
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
+              <Route index element={<Navigate replace to='dashboard' />} />
+              <Route path='dashboard' element={<Dashboard />} />
+              <Route path='bookings' element={<Bookings />} />
+              <Route path='bookings/:bookingId' element={<Booking />} />
+              <Route path='checkin/:bookingId' element={<Checkin />} />
 
-            <Route path='cabins' element={<Cabins />} />
-            <Route path='users' element={<Users />} />
-            <Route path='settings' element={<Settings />} />
-            <Route path='account' element={<Account />} />
-          </Route>
+              <Route path='cabins' element={<Cabins />} />
+              <Route path='users' element={<Users />} />
+              <Route path='settings' element={<Settings />} />
+              <Route path='account' element={<Account />} />
+            </Route>
 
-          <Route path='login' element={<Login />} />
-          <Route path='*' element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
-      <ReactQueryDevtools initialIsOpen={false} />
-      <Toaster
-        position='top-center'
-        gutter={12}
-        containerStyle={{
-          margin: '8px',
-        }}
-        toastOptions={{
-          success: {
-            duration: 8000,
-          },
-          error: {
-            duration: 5000,
-          },
-          style: {
-            fontSize: '16px',
-            maxWidth: '500px',
-            padding: '16px 24px',
-            backgroundColor: 'var(--color-grey-0)',
-            color: 'var(--color-grey-700)',
-          },
-        }}
-      />
-    </QueryClientProvider>
+            <Route path='login' element={<Login />} />
+            <Route path='*' element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <Toaster
+          position='top-center'
+          gutter={12}
+          containerStyle={{
+            margin: '8px',
+          }}
+          toastOptions={{
+            success: {
+              duration: 8000,
+            },
+            error: {
+              duration: 5000,
+            },
+            style: {
+              fontSize: '16px',
+              maxWidth: '500px',
+              padding: '16px 24px',
+              backgroundColor: 'var(--color-grey-0)',
+              color: 'var(--color-grey-700)',
+            },
+          }}
+        />
+      </QueryClientProvider>
+    </DarkModePovider>
   )
 }
 
