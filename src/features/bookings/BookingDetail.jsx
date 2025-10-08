cb7d091ba0c07fb17734de84a9b62543f6b13b63
Oleh Zinchenko <olegzinchenko1983@gmail.com>
2025-10-08 14:30:45 +0100

    Fixed bug with BookingDetail page

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -16,6 +16,7 @@ import { useMoveBack } from '../../hooks/useMoveBack'
 import { useBooking } from './useBooking'
 import { useCheckout } from '../check-in-out/useCheckout'
 import { useDeleteBooking } from './useDeleteBooking'
+import Empty from '../../ui/Empty'
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -31,6 +32,7 @@ function BookingDetail() {
   const navigate = useNavigate()
 
   if (isPending) return <Spinner />
+  if (!booking) return <Empty resourceName='booking' />
 
   const { status, id: bookingId } = booking
 
